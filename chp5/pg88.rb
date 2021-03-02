c5e9679e2dc6928b2d3153facfc8506d402311f6
Njunu-sk <simonnjunu16@gmail.com>
2021-03-02 09:13:03 +0300

    minor update

--- chp5/pg88.rb
+++ chp5/pg88.rb
@@ -21,7 +21,7 @@ end
 
 class Mechanic
   def prepare_bicycles(bicycles)
-    bicycles.each {|bicycle| prepare_bicycle(bicycle) }
+    bicycles.each { |bicycle| prepare_bicycle(bicycle) }
   end
 
   def prepare_bicycle(bicycle)
